47af1ee759d538927c8ad9cb9940d5602c3d156c
Rishi | RRR <73101003+Rishiraj0100@users.noreply.github.com>
2025-05-28 17:01:22 +0530

    Update __init__.py

--- shapesinc/__init__.py
+++ shapesinc/__init__.py
@@ -1,5 +1,19 @@
-from .abc import ShapeUser, ShapeChannel, Message, MessageContent, PromptResponse, ContentType
-from .shape import Shape, AsyncShape
+from .abc import (
+  ShapeUser,
+  ShapeChannel,
+   Message,
+  MessageContent,
+  PromptResponse,
+  ContentType,
+  APIError,
+  RateLimitError
+)
+from .shape import (
+  ShapeBase,
+  Shape,
+  AsyncShape,
+  shape
+)
 
 
 __version__ = "0.1.0b"
