889867cef242f0e04ebd439048f31ad0dab342fd
Rishi | RRR <73101003+Rishiraj0100@users.noreply.github.com>
2025-06-01 14:15:54 +0530

    v0.2.0b

--- shapesinc/shape.py
+++ shapesinc/shape.py
@@ -1,10 +1,4 @@
-import aiohttp
-import json
-import sys
 import typing
-import urllib.request as request
-
-from urllib.error import HTTPError
 
 from .__info__ import __version__
 from .abc import (
@@ -12,42 +6,41 @@ from .abc import (
   ShapeChannel as Channel,
   Message,
   PromptResponse,
+  MISSING,
+  TypedDict
+)
+from .http import (
+  Route,
+  AsyncRoute,
   APIError,
   RateLimitError
 )
 
-vi = sys.version_info
-PYTHON_VERSION = f"{vi.major}.{vi.minor}.{vi.micro}{vi.releaselevel}{vi.serial}".split("final")[0]
-
-BASE_HTTP_URL = "https://api.shapes.inc/v1"
-
-SYNCHRONOUS_MODELS = {}
-ASYNCHRONOUS_MODELS = {}
-ACTIVE_MODELS = {
-  "SYNCHRONOUS": SYNCHRONOUS_MODELS,
-  "ASYNCHRONOUS": ASYNCHRONOUS_MODELS
-}
 
 class ShapeBase:
-  """
-  Base class for creating shapes.
+  """Base class for creating shapes.
+  
   Parameters
   ------------
   api_key: :class:`~str`
     Your API key for shapes.inc
   username: :class:`~str`
     Username of the shape
-    
-  Raises
-  -------
-  RuntimeError
-    It is raised when an instance for given username is already created for same environment.
+  app_id: Optional[:class:`~str`]
+    Application ID of the shape.
   """
-  def __init__(self, api_key: str, username: str):
-    assert username not in ACTIVE_MODELS[self.type], RuntimeError(f"An instance ({self.type.lower()}) of model '{username}' has already been created for this environment. Please use that instance instead")
-    ACTIVE_MODELS[self.type][username] = self
+  def __init__(self, api_key: str, username: str, app_id: str = MISSING):
     self.api_key = api_key
     self.username = username
+    self.app_id = app_id
+    
+  @property
+  def auth_url(self) -> str:
+    """Authorization URL of the shape"""
+    if self.app_id is MISSING:
+      raise ValueError("app_id was not found for the given shape.")
+      
+    return Route.SITE/"authorize?app_id="+self.app_id
 
   @property
   def type(self) -> typing.Literal["SYNCHRONOUS", "ASYNCHRONOUS"]:
@@ -68,21 +61,37 @@ class ShapeBase:
     """Name of model of your shape"""
     return "shapesinc/"+self.username
     
-  def prompt(self, message: Message, user: User = None, channel: Channel = None) -> typing.Union[PromptResponse, typing.Awaitable[PromptResponse]]:
+  def prompt(
+    self,
+    message: Message,
+    user: User = None,
+    channel: Channel = None
+  ) -> typing.Union[PromptResponse, typing.Awaitable[PromptResponse]]:
     headers = {
       "Authorization": f"Bearer {self.api_key}",
       "Content-Type": "application/json"
     }
+    if self.app_id is not MISSING:
+      headers["X-App-ID"] = self.app_id
     if user is not None:
       headers["X-User-Id"] = user.id
+      if user.auth_token and self.app_id is not MISSING:
+        headers["X-User-Auth"] = user.auth_token
+        del headers["Authorization"]
+        
     if channel is not None:
       headers["X-Channel-Id"] = channel.id
       
     if isinstance(message, str):
       message=Message.new(message)
-    return self.make_request([message.to_dict()], headers)
+      
+    return self.make_send_message_request([message.to_dict()], headers)
 
-  def make_request(self, message: typing.List[typing.Dict[str, str]], headers: typing.Dict[str, str]) -> PromptResponse:
+  def make_send_message_request(
+    self,
+    messages: typing.List[typing.Dict[str, str]],
+    headers: typing.Dict[str, str]
+  ) -> PromptResponse:
     """The method which is implemented to make requests to API
 
     Raises
@@ -102,6 +111,8 @@ class Shape(ShapeBase):
     Your API key for shapes.inc
   username: :class:`~str`
     Username of the shape
+  app_id: Optional[:class:`~str`]
+    Application ID of the shape.
 
   Example
   ---------
@@ -116,10 +127,6 @@ class Shape(ShapeBase):
           q = input(" >>> ")
           print(shape.prompt(q))
 
-  Raises
-  -------
-  RuntimeError
-    It is raised when an instance for given username is already created for same environment.
   """
   type: str = "SYNCHRONOUS"
   
@@ -139,6 +146,7 @@ class Shape(ShapeBase):
     --------
     :class:`shapesinc.PromptResponse`
       The response of the prompt.
+      
     Raises
     -------
     :class:`shapesinc.RateLimitError`
@@ -148,25 +156,30 @@ class Shape(ShapeBase):
     """
     return super().prompt(*args, **kwargs)
 
-  def make_request(self, messages: typing.List[typing.Dict[str, str]], headers: typing.Dict[str, str]) -> PromptResponse:
-    headers["User-Agent"] = f"Shapes.inc API Wrapper Py using Python/{PYTHON_VERSION} shapesinc-py/{__version__}"
-    req = request.Request(
-      BASE_HTTP_URL+"/chat/completions",
-      data = json.dumps({
+  def make_send_message_request(
+    self,
+    messages: typing.List[typing.Dict[str, str]],
+    headers: typing.Dict[str, str]
+  ) -> PromptResponse:
+    return PromptResponse(shape=self, **(Route.API_BASE/"chat/completions").request(
+      "POST",
+      headers,
+      {
         "model": self.model_name,
         "messages": messages
-      }).encode(),
-      headers = headers
-    )
-    try:
-      req = request.urlopen(req)
-    except HTTPError as req:
-      res = json.loads(req.read())
-      e = RateLimitError if req.code == 429 else APIError
-      raise e(res)
-
-    res = json.loads(req.read())
-    return PromptResponse(shape=self, **res)
+      }
+    ))
+    
+  def info(self) -> dict:
+    """Tells information about shape.
+    
+    Returns
+    --------
+    :class:`~dict`
+      Reponse from API
+    """
+    res = (Route/"shapes/public"/self.username).request("GET")
+    return TypedDict(**res)
 
 
 class AsyncShape(ShapeBase):
@@ -180,6 +193,8 @@ class AsyncShape(ShapeBase):
     Your API key for shapes.inc
   username: :class:`~str`
     Username of the shape
+  app_id: Optional[:class:`~str`]
+    Application ID of the shape.
 
   Example
   ---------
@@ -194,10 +209,6 @@ class AsyncShape(ShapeBase):
           q = input(" >>> ")
           print(await shape.prompt(q))
 
-  Raises
-  -------
-  RuntimeError
-    It is raised when an instance for given username is already created for same environment.
   """
   type: str = "ASYNCHRONOUS"
   
@@ -225,27 +236,41 @@ class AsyncShape(ShapeBase):
       Error raised by shapes.inc API.
     """
     return await super().prompt(*args, **kwargs)
-  async def make_request(self, messages: typing.List[typing.Dict[str, str]], headers: typing.Dict[str, str]) -> PromptResponse:
-    async with aiohttp.ClientSession() as ses:
-      data = await ses.post(
-        BASE_HTTP_URL+"/chat/completions",
-        json = {
-          "model": self.model_name,
-          "messages": messages
-        },
-        headers = headers
-      )
-      sc = data.status
-      res = await data.json()
-      
-    if sc == 429:
-      raise RateLimitError(res)
-    if "error" in res:
-      raise APIError(res)
-    return PromptResponse(shape=self, **res)
+
+  async def make_send_message_request(
+    self,
+    messages: typing.List[typing.Dict[str, str]],
+    headers: typing.Dict[str, str]
+  ) -> PromptResponse:
+    req = await (AsyncRoute.API_BASE/"chat/completions").request(
+      "POST",
+      headers,
+      {
+        "model": self.model_name,
+        "messages": messages
+      }
+    )
+    return PromptResponse(shape=self, **req)
+    
+  async def info(self) -> dict:
+    """Tells information about shape.
+    
+    Returns
+    --------
+    :class:`~dict`
+      Reponse from API
+    """
+    res = await (AsyncRoute/"shapes/public"/self.username).request("GET")
+    return TypedDict(**res)
 
 
-def shape(api_key: str, username: str, *, synchronous: bool = True) -> typing.Union[Shape, AsyncShape]:
+def shape(
+  api_key: str,
+  username: str,
+  app_id: str = None,
+  *,
+  synchronous: bool = True
+) -> typing.Union[Shape, AsyncShape]:
   """Creates a new instance for a shape if none exists; otherwise, returns the original instance.
 
   Parameters
@@ -254,22 +279,16 @@ def shape(api_key: str, username: str, *, synchronous: bool = True) -> typing.Un
     Your API key for shapes.inc
   username: :class:`~str`
     Username of the shape
+  app_id: Optional[:class:`~str`]
+    Application ID of the shape.
   synchronous: Optional[:class:`~bool`]
-    Whether the instance is to be configured for synchronous environment or asynchronous. Default: False
+    Whether the instance is to be configured for synchronous environment or asynchronous. Default: True
   
   Returns
   --------
   :class:`shapesinc.Shape`
-    If the instance is configured for synchronous environment.
+    If the value of synchronous is set to True.
   :class:`shapesinc.AsyncShape`
-    If the instance is configured for asynchronous environment.
+    If the value of synchronous is set to False.
   """
-  type = "ASYNCHRONOUS" if not synchronous else "SYNCHRONOUS"
-  model = ACTIVE_MODELS[type].get(username)
-  if model:
-    return model
-    
-  if synchronous:
-    return Shape(api_key, username)
-    
-  return AsyncShape(api_key, username)
+  return (Shape if synchronous else AsyncShape)(api_key, username, app_id)
